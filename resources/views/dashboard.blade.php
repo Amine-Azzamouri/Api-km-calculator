824a7fdf2b83c1f9c39899f9bfc4aad8a7553aee
Amine-Azzamouri <334491@student.mboutrecht.nl>
2024-04-15 19:40:02 +0200

    initial commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -96,64 +96,6 @@
                 </head>
                     <body>
                         <div class="p-6 text-gray-900 dark:text-gray-100">
-                            <h2>Profiel</h2>
-                            <form id="profileForm">
-                                <label for="companyName"></label>
-                                <input type="text" id="companyName" name="companyName" required placeholder="Voer de bedrijfsnaam in">
-
-                                <label for="address"></label>
-                                <input type="text" id="address" name="address" required placeholder="Voer de straat en huisnummer in">
-
-                                <label for="postcode"></label>
-                                <input type="text" id="postcode" name="postcode" required placeholder="Voer de postcode in">
-
-                                <label for="city"></label>
-                                <input type="text" id="city" name="city" required placeholder="Voer de plaats in">
-
-                                <label for="country"></label>
-                                <input type="text" id="country" name="country" required placeholder="Voer het land in">
-
-                                <label for="kvkNumber"></label>
-                                <input type="text" id="kvkNumber" name="kvkNumber" required placeholder="Voer het KVK-nummer in">
-
-                                <label for="phoneNumber"></label>
-                                <input type="text" id="phoneNumber" name="phoneNumber" required placeholder="Voer het telefoonnummer in">
-                                </br>
-                                <input type="submit" value="Opslaan">
-                                </br>
-                                </br>
-                            </form>
-                                <div class="container">
-                                <h1>Ritten Dashboard</h1>
-
-                                <div class="dashboard">
-                                    <div class="dashboard-item">
-                                        <h2>Uitgevoerde Ritten</h2>
-                                        <ul>
-                                            <li class="completed">Rit 1</li>
-                                            <li class="completed">Rit 2</li>
-                                            <li class="completed">Rit 3</li>
-                                        </ul>
-                                    </div>
-
-                                    <div class="dashboard-item">
-                                        <h2>Lopende Ritten</h2>
-                                        <ul>
-                                            <li class="ongoing">Rit 4</li>
-                                            <li class="ongoing">Rit 5</li>
-                                        </ul>
-                                    </div>
-
-                                    <div class="dashboard-item">
-                                        <h2>Geplande Ritten</h2>
-                                        <ul>
-                                            <li class="planned">Rit 6</li>
-                                            <li class="planned">Rit 7</li>
-                                            <li class="planned">Rit 8</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
                             <form id="bookingForm" method="POST" action="getMiles">
                                 @csrf
                                 <label for="pickupAddress">Ophaaladres:</label>
